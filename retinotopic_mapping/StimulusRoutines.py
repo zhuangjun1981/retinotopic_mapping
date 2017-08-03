0d1f31f288080dfc38c645697e7c4df5c0e7ef1e
John H. Yearsley <yearsj@uw.edu>
2017-08-03 08:34:25 -0700

    Dev make one method for display by index (#5)
    pooled commits from John
    
    * debug docs4
    
    * debug docs 5
    
    * debug docs 6
    
    * debug docs 6
    
    * Add retinotopic map video to docs
    
    * debug sphinx-docs
    
    * add _static path to conf.py
    
    * push new html builds to github
    
    * update docs
    
    * debug docs
    
    * debug docs 2
    
    * debug docs 3
    
    * debug docs 3
    
    * debug docs 4
    
    * debug docs 5
    
    * debug docs 5
    
    * change remap() doc method
    
    * first iteration that can display by index
    
    * clean up some code
    
    * add some tests for timing purposes
    
    * Add timing tests for index display
    
    * fix typos
    
    * Fix DriftingGratingCircle indicator bug and reorganize seq_log variables
    
    * clean up a bit
    
    * delete old code that was commented out
    
    * Rewrite index display so that there is one routine
    
    * clean up by deleting commented code
    
    * Fix display by index bug where exit key was not stopping display

--- retinotopic_mapping/StimulusRoutines.py
+++ retinotopic_mapping/StimulusRoutines.py
@@ -329,15 +329,15 @@ class UniformContrast(Stim):
         frames = ((0,-1), (1,1.), (0,-1))
         
         #Number of times each respective parameter will be displayed
-        num_iters = (self.pregap_frame_num, 
+        num_disp_iters = (self.pregap_frame_num, 
                      display_frame_num, 
                      self.postgap_frame_num)
         
-        return frames, num_iters
+        return frames, num_disp_iters
     
     def generate_movie_by_index(self):
         """ by index """
-        self.frames, num_iters = self.generate_frames_by_index()
+        self.frames, num_disp_iters = self.generate_frames_by_index()
         
         num_unique_frames = len(self.frames)
         num_pixels_width = self.monitor.deg_coord_x.shape[0]
@@ -367,6 +367,8 @@ class UniformContrast(Stim):
                                       num_pixels_height),
                                       dtype=np.float16)
         
+        index_to_display = []
+        
         for i in range(num_unique_frames):
             curr_frame = self.frames[i]
             
@@ -381,9 +383,13 @@ class UniformContrast(Stim):
             
             full_sequence[i] = stim_sequence
             
+            for reps in range(num_disp_iters[i]):
+                index_to_display.append(i)
+            
         monitor_dict = dict(self.monitor.__dict__)
         indicator_dict = dict(self.indicator.__dict__)
         NF_dict = dict(self.__dict__)
+        NF_dict['index_to_display'] = index_to_display
         NF_dict.pop('monitor')
         NF_dict.pop('indicator')
         full_dict = {'stimulation' : NF_dict,
@@ -391,7 +397,7 @@ class UniformContrast(Stim):
                      'indicator' : indicator_dict}
 
         
-        return full_sequence, None, num_iters, full_dict
+        return full_sequence, full_dict
 
     def generate_movie(self):
         """
@@ -619,10 +625,10 @@ class FlashingCircle(Stim):
         # compute unique frame parameters
         self.frames = self.generate_frames_by_index()
         
-        # sum(num_repeats) == number of total frames in one iteration.
-        # each value of num_repeats corresponds to the number of times
+        # sum(num_disp_iters) == number of total frames in one iteration.
+        # each value of num_disp_iters corresponds to the number of times
         # to display each respective unique frame.
-        num_repeats = (1,
+        num_disp_iters = (1,
                        self.pregap_frame_num-1,
                        self.flash_frame,
                        self.postgap_frame_num)
@@ -662,6 +668,8 @@ class FlashingCircle(Stim):
         circle_mask = get_circle_mask(map_x, map_y, 
                                   self.center, self.radius).astype(np.float16)
         
+        index_to_display = []
+        
         for i in range(num_unique_frames):
             curr_frame = self.frames[i]
             
@@ -675,17 +683,21 @@ class FlashingCircle(Stim):
             
             full_sequence[i] = curr_FC_seq
             
+            for reps in range(num_disp_iters[i]):
+                index_to_display.append(i)
+        
         mondict=dict(self.monitor.__dict__)
         indicator_dict=dict(self.indicator.__dict__)
         indicator_dict.pop('monitor')
         NFdict=dict(self.__dict__)
+        NFdict['index_to_display'] = index_to_display
         NFdict.pop('monitor')
         NFdict.pop('indicator')
         full_dict={'stimulation':NFdict,
                    'monitor':mondict,
                    'indicator':indicator_dict}
 
-        return full_sequence, None, num_repeats, full_dict
+        return full_sequence, full_dict
 
     def generate_movie(self):
         """
@@ -1034,7 +1046,7 @@ class SparseNoise(Stim):
     
     def generate_frames_by_index(self):
         frames = []
-        num_repeats = []
+        num_disp_iters = []
         
         off_params = [0,None,None,-1]
         
@@ -1059,21 +1071,21 @@ class SparseNoise(Stim):
             if self.pregap_frame_num != 0:
                 # then add off_params to frames
                 frames.append(off_params)
-                num_repeats.append(self.pregap_frame_num)
+                num_disp_iters.append(self.pregap_frame_num)
             
             iter_grid_points = self._generate_grid_points_sequence()
             
             for grid_point in iter_grid_points:
                 frames.append([1,grid_point[0],grid_point[1],1])
-                num_repeats.append(indicator_on_frame)
+                num_disp_iters.append(indicator_on_frame)
             
                 frames.append([1,grid_point[0],grid_point[1],-1])
-                num_repeats.append(indicator_off_frame)
+                num_disp_iters.append(indicator_off_frame)
                 
             if self.postgap_frame_num != 0:
                 # then add off_params to frames
                 frames.append(off_params)
-                num_repeats.append(self.postgap_frame_num)
+                num_disp_iters.append(self.postgap_frame_num)
                 
         """ Might be difficult to add in non synchronized indicator without
         first generating all of the frames. Is this important?"""
@@ -1081,10 +1093,10 @@ class SparseNoise(Stim):
         frames = tuple(frames)
         frames = [tuple(x) for x in frames]
         
-        return tuple(frames), num_repeats
+        return tuple(frames), num_disp_iters
     
     def generate_movie_by_index(self):
-        self.frames, num_repeats = self.generate_frames_by_index()
+        self.frames, num_disp_iters = self.generate_frames_by_index()
         
         num_unique_frames = len(self.frames)
         num_pixels_width = self.monitor.deg_coord_x.shape[0]
@@ -1110,6 +1122,7 @@ class SparseNoise(Stim):
                                              num_pixels_width,
                                              num_pixels_height),
                                              dtype=np.float16)
+        index_to_display = []
         
         for i, frame in enumerate(self.frames):
             if frame[0] == 1:
@@ -1122,25 +1135,26 @@ class SparseNoise(Stim):
                                              foreground_color=frame[2],
                                              background_color=self.background)
                 
-                """ Lots of calulations are done in the original routine, what 
-                are they for? Are they necessary, do they speed something up?"""
-                
                 full_seq[i] = disp_mat
             
             full_seq[i, indicator_height_min:indicator_height_max,
                      indicator_width_min:indicator_width_max] = frame[3]
             
+            for reps in range(num_disp_iters[i]):
+                index_to_display.append(i)
+            
         mondict=dict(self.monitor.__dict__)
         indicator_dict=dict(self.indicator.__dict__)
         indicator_dict.pop('monitor')
         SNdict=dict(self.__dict__)
+        SNdict['index_to_display'] = index_to_display
         SNdict.pop('monitor')
         SNdict.pop('indicator')
         full_dict={'stimulation':SNdict,
                         'monitor':mondict,
                         'indicator':indicator_dict}
             
-        return full_seq, None, num_repeats, full_dict
+        return full_seq, full_dict
                     
             
 
@@ -1491,13 +1505,13 @@ class DriftingGratingCircle(Stim):
         
         # used to store number of repeats for each frame, e.g. pregap_frame_num
         # postgap_frame_num, block_frame_num, ...
-        num_frame_repeats = [self.pregap_frame_num]
+        num_disp_iters = [self.pregap_frame_num]
         
         
         for i in range(self.iteration):
             if i!=0:
                 num_unique_block_frames.append(1)
-                num_frame_repeats.append(midgap_frame_num)
+                num_disp_iters.append(midgap_frame_num)
             single_run_frames += [off_params]
             
             # Compute all combinations of defining parameters
@@ -1507,7 +1521,7 @@ class DriftingGratingCircle(Stim):
                 if j!=0:
                     single_run_frames += [off_params]
                     num_unique_block_frames.append(1)
-                    num_frame_repeats.append(midgap_frame_num)
+                    num_disp_iters.append(midgap_frame_num)
                 sf, tf, dire, con, size = condition
                 
                 # Compute phase list
@@ -1533,26 +1547,16 @@ class DriftingGratingCircle(Stim):
                         stim_on += [(1, sf, tf, dire, con, size, phase, 0.)]
                         
                 single_run_frames += stim_on
-                num_frame_repeats.append(block_gap_num)
+                num_disp_iters.append(block_gap_num)
                 
         
         single_run_frames += [off_params]
         num_unique_block_frames.append(1)
-        num_frame_repeats.append(self.postgap_frame_num)
-        
-        return single_run_frames, num_unique_block_frames, num_frame_repeats
+        num_disp_iters.append(self.postgap_frame_num)
+               
+        return single_run_frames, num_unique_block_frames, num_disp_iters
     
     def generate_movie_by_index(self):
-        """ THERE IS A BUG THAT SHOULD BE FIXED! RIGHT NOW THE INDICATOR
-        COORDINATES ARE FLIPPED SO THAT NORTH=SOUTH AND VICE VERSA
-        
-        
-        Can still be optimized further. As of now the code is still storing
-        repeated frames when there are multiple iterations in a given 
-        initialization. this could be changed so that multiple iterations are 
-        not stored.
-        
-        """
         
         (self.frames, 
          num_unique_block_frames, num_disp_iters) = self.generate_frames_by_index()
@@ -1617,17 +1621,42 @@ class DriftingGratingCircle(Stim):
             mov[i, indicator_height_min:indicator_height_max,
                 indicator_width_min:indicator_width_max] = frame[-1]
             
+        # Compute list of indices of each frame to display
+        index_to_display = []
+        
+        cumsum = np.cumsum(num_unique_block_frames)
+        cumsum = list(np.insert(cumsum,0,0))
+        
+        frame_blocks = []
+        
+        for i in range(len(cumsum)-1):
+            frame_blocks.append(range(cumsum[i],cumsum[i+1]))
+            
+        for n,frame_block in enumerate(frame_blocks):
+            if len(frame_block)==1:
+                index_to_display += frame_block*num_disp_iters[n]
+            else:
+                repeats = (num_disp_iters[n] / num_unique_block_frames[n]) + 1
+                
+                repeated_block = frame_block*repeats
+                
+                repeated_block = repeated_block[:num_disp_iters[n]]
+                
+                index_to_display += repeated_block
+        
+            
         mondict=dict(self.monitor.__dict__)
         indicator_dict=dict(self.indicator.__dict__)
         indicator_dict.pop('monitor')
         self_dict=dict(self.__dict__)
+        self_dict['index_to_display'] = index_to_display
         self_dict.pop('monitor')
         self_dict.pop('indicator')
         log={'stimulation':self_dict,
              'monitor':mondict,
              'indicator':indicator_dict}    
             
-        return mov, num_unique_block_frames, num_disp_iters, log
+        return mov, log
             
 
 
