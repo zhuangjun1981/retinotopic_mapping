c1451e80faf656dde127c5cc28f2271bd48e9167
John Yearsley <yearsj@uw.edu>
2017-08-24 13:45:29 -0700

    Clean up stim routines again

--- retinotopic_mapping/StimulusRoutines.py
+++ retinotopic_mapping/StimulusRoutines.py
@@ -301,7 +301,7 @@ class UniformContrast(Stim):
 
     def generate_frames(self):
         """
-        generate a tuple of parameters of each frame.
+        generate a tuple of parameters with information for each frame.
 
         Information contained in each frame:
              first element -
@@ -314,21 +314,19 @@ class UniformContrast(Stim):
 
         displayframe_num = int(self.duration * self.monitor.refresh_rate)
 
-        frames = [(0, -1.)] * self.pregap_frame_num + \
-                 [(1, 1.)] * displayframe_num + \
-                 [(0, -1.)] * self.postgap_frame_num
+        frames = [(0., -1.)] * self.pregap_frame_num + \
+                 [(1., 1.)] * displayframe_num + \
+                 [(0., -1.)] * self.postgap_frame_num
                   
 
         return tuple(frames)
     
     
     def _generate_frames_for_index_display(self):
-        
+        " parameters are predefined here, nothing to compute. "
         if self.indicator.is_sync:
-            display_frame_num = int(self.duration * self.monitor.refresh_rate)
-        
             # Parameters that define the stimulus
-            frames = ((0,-1), (1,1.), (0,-1))
+            frames = ((0.,-1.), (1.,1.), (0.,-1.))
         
             return frames
         
@@ -336,7 +334,7 @@ class UniformContrast(Stim):
             raise NotImplementedError, "method not avaialable for non-sync indicator"
     
     def _generate_display_index(self):
-        
+        """ compute a list of indices corresponding to each frame to display. """
         display_frame_num = int(self.duration * self.monitor.refresh_rate)
     
         #Number of times each respective parameter will be displayed
@@ -355,7 +353,7 @@ class UniformContrast(Stim):
     
     
     def generate_movie_by_index(self):
-        """ by index """
+        """ compute the stimulus movie to be displayed by index. """
         self.frames, self.index_to_display = self._generate_display_index()
         
         num_frames = len(self.frames)
@@ -386,7 +384,6 @@ class UniformContrast(Stim):
                                       num_pixels_height),
                                       dtype=np.float16)
         
-        
         for i in range(num_frames):
             curr_frame = self.frames[i]
             
@@ -410,19 +407,18 @@ class UniformContrast(Stim):
                      'monitor' : monitor_dict,
                      'indicator' : indicator_dict}
 
-        
         return full_sequence, full_dict
 
     def generate_movie(self):
         """
-        generate movie for uniform contrast display frame by frame
+        generate movie for uniform contrast display frame by frame.
 
         Returns
         -------
-        full_seq : list
-            3-d array of the stimulus to be displayed. elements are of type unit8
+        full_seq : nd array, uint8
+            3-d array of the stimulus to be displayed. 
         full_dict : dict
-            dictionary containing the information of the stimulus
+            dictionary containing the information of the stimulus.
         """
 
         self.frames = self.generate_frames()
@@ -491,27 +487,27 @@ class FlashingCircle(Stim):
     Parameters
     ----------
     monitor : monitor object
-        contains display monitor information
+        contains display monitor information.
     indicator : indicator object
-        contains indicator information
+        contains indicator information.
     coordinate : str from {'degree','linear'}, optional
-        specifies coordinates, defaults to 'degree'
+        specifies coordinates, defaults to 'degree'.
     background : float, optional
         color of background. Takes values in [-1,1] where -1 is black and 1
-        is white
+        is white.
     stim_name : str
-        Name of the stimulus
+        Name of the stimulus.
     center : 2-tuple, optional
-        center coordinate of the circle in degrees, defaults to `(90.,10.)`
+        center coordinate of the circle in degrees, defaults to `(90.,10.)`.
     radius : float, optional
         radius of the circle, defaults to `10.`
     color : float, optional
         color of the circle, takes values in [-1,1], defaults to `-1.`
     iteration : int, optional
-        total number of flashes, defaults to `1`
+        total number of flashes, defaults to `1`.
     flash_frame : int, optional
         number of frames that circle is displayed during each presentation
-        of the stimulus, defaults to `3`
+        of the stimulus, defaults to `3`.
     """
     def __init__(self,
                  monitor,
@@ -567,7 +563,7 @@ class FlashingCircle(Stim):
     def generate_frames(self):
         """
         function to generate all the frames needed for the stimulation.
-        Returns a list of information of all frames as a list of tuples
+       
 
         Information contained in each frame:
            first element :
@@ -582,6 +578,10 @@ class FlashingCircle(Stim):
                 corresponds to the color of indicator and during stimulus
                 the value is equal to 1, whereas during a gap the value is
                 equal to 0
+        Returns
+        -------
+        frames : list
+            list of information defining each frame.
         """
 
         # number of frames for one round of stimulus
@@ -636,9 +636,6 @@ class FlashingCircle(Stim):
         else: 
             raise NotImplementedError, "method not available for non-sync indicator"
         
-        
-        
-    
     def _generate_display_index(self):
         
         frames = self._generate_frames_for_index_display()
@@ -1270,6 +1267,212 @@ class SparseNoise(Stim):
                         'indicator':indicator_dict}
 
         return full_seq, full_dict
+    
+    
+class LocallySparseNoise(Stim):
+    
+    
+    def __init__(self,
+                 monitor,
+                 indicator,
+                 background=0.,
+                 coordinate='degree',
+                 grid_space=(10.,10.),
+                 probe_size=(10.,10.),
+                 probe_orientation=0.,
+                 probe_frame_num=6,
+                 subregion=None,
+                 sign='ON-OFF',
+                 iteration=1,
+                 pregap_dur=2.,
+                 postgap_dur=3.,
+                 is_include_edge=True):
+        
+        super(SparseNoise,self).__init__(monitor=monitor,
+                                         indicator=indicator,
+                                         background=background,
+                                         coordinate=coordinate,
+                                         pregap_dur=pregap_dur,
+                                         postgap_dur=postgap_dur)
+        """    
+        Initialize locally sparse noise object, inherits Parameters from Stim object
+        """
+
+        self.stim_name = 'LocallySparseNoise'
+        self.grid_space = grid_space
+        self.probe_size = probe_size
+        self.probe_orientation = probe_orientation
+        self.probe_frame_num = probe_frame_num
+        self.is_include_edge = is_include_edge
+        self.frame_config = ('is_display', '(azimuth, altitude)',
+                             'polarity', 'indicator_color')
+
+        if subregion is None:
+            if self.coordinate == 'degree':
+                self.subregion = [np.amin(self.monitor.deg_coord_y),
+                                  np.amax(self.monitor.deg_coord_y),
+                                  np.amin(self.monitor.deg_coord_x),
+                                  np.amax(self.monitor.deg_coord_x)]
+            if self.coordinate == 'linear':
+                self.subregion = [np.amin(self.monitor.lin_coord_y),
+                                  np.amax(self.monitor.lin_coord_y),
+                                  np.amin(self.monitor.lin_coord_x),
+                                  np.amax(self.monitor.lin_coord_x)]
+        else:
+            self.subregion = subregion
+
+        self.sign = sign
+        self.iteration = iteration
+
+
+        
+
+        
+        self.clear()
+        
+    def _getgrid_points(self, is_plot=False):
+        """
+        generate all the grid points in display area (covered by both subregion and
+        monitor span)
+
+        Returns
+        -------
+        grid_points : n x 2 array, 
+            refined [azi, alt] pairs of probe centers going to be displayed
+        """
+
+        rows = np.arange(self.subregion[0], 
+                         self.subregion[1] + self.grid_space[0], 
+                         self.grid_space[0])
+        columns = np.arange(self.subregion[2], 
+                            self.subregion[3] + self.grid_space[1], 
+                            self.grid_space[1])
+
+        xx, yy = np.meshgrid(columns, rows)
+
+        gridPoints = np.transpose(np.array([xx.flatten(), yy.flatten()]))
+
+        # get all the visual points for each pixels on monitor
+        if self.coordinate == 'degree':
+            monitor_x = self.monitor.deg_coord_x
+            monitor_y = self.monitor.deg_coord_y
+        elif self.coordinate == 'linear':
+            monitor_x = self.monitor.lin_coord_x
+            monitor_y = self.monitor.lin_coord_y
+        else:
+            raise ValueError('Do not understand coordinate system: {}. Should be either "linear" or "degree".'.
+                             format(self.coordinate))
+
+        left_alt = monitor_y[:, 0]
+        right_alt = monitor_y[:, -1]
+        top_azi = monitor_x[0, :]
+        bottom_azi = monitor_x[-1, :]
+
+        left_azi = monitor_x[:, 0]
+        right_azi = monitor_x[:, -1]
+        top_alt = monitor_y[0, :]
+        bottom_alt = monitor_y[-1, :]
+
+        left_azi_e = left_azi - self.grid_space[1]
+        right_azi_e = right_azi + self.grid_space[1]
+        top_alt_e = top_alt + self.grid_space[0]
+        bottom_alt_e = bottom_alt - self.grid_space[0]
+
+        all_alt = np.concatenate((left_alt, right_alt, top_alt, bottom_alt))
+        all_azi = np.concatenate((left_azi, right_azi, top_azi, bottom_azi))
+
+        all_alt_e = np.concatenate((left_alt, right_alt, top_alt_e, bottom_alt_e))
+        all_azi_e = np.concatenate((left_azi_e, right_azi_e, top_azi, bottom_azi))
+
+        monitorPoints = np.array([all_azi, all_alt]).transpose()
+        monitorPoints_e = np.array([all_azi_e, all_alt_e]).transpose()
+
+        # get the grid points within the coverage of monitor
+        if self.is_include_edge:
+            gridPoints = gridPoints[in_hull(gridPoints, monitorPoints_e)]
+        else:
+            gridPoints = gridPoints[in_hull(gridPoints, monitorPoints)]
+
+        if is_plot:
+            f = plt.figure()
+            ax = f.add_subplot(111)
+            ax.plot(monitorPoints[:, 0], monitorPoints[:, 1], '.r', label='monitor')
+            ax.plot(monitorPoints_e[:, 0], monitorPoints_e[:, 1], '.g', label='monitor_e')
+            ax.plot(gridPoints[:, 0], gridPoints[:, 1], '.b', label='grid')
+            ax.legend()
+            plt.show()
+
+        return gridPoints
+        
+        
+    @staticmethod
+    def exclude_region(av,(x,y),exclusion=0):
+
+        X,Y = np.meshgrid(np.arange(av.shape[0]), np.arange(av.shape[1]))
+
+        mask = ((X-x)**2 + (Y-y)**2) <= exclusion**2
+        av[mask] = False
+     
+    @staticmethod    
+    def exclude_threshold(av,pixel_off_count_grid, pixel_on_count_grid, repeats):
+    
+        count_mask_off = pixel_off_count_grid == repeats
+        av[count_mask_off] = False
+          
+        count_mask_on = pixel_on_count_grid == repeats
+        av[count_mask_on] = False
+    
+    
+    @classmethod
+    def create_sparse_noise_matrix(cls,X=16,Y=28,exclusion=5,T=9000, 
+                                   buffer_x=6, buffer_y=6, pixel_repeats=10):
+
+        X_extended = X+2*buffer_x
+        Y_extended = Y+2*buffer_y
+        
+        
+        extended_pixel_grid = [X_extended, Y_extended]
+        pixel_off_count_grid = np.zeros(extended_pixel_grid, dtype=np.uint8)
+        pixel_on_count_grid = np.zeros(extended_pixel_grid, dtype=np.uint8)
+        target_pixel_count = pixel_repeats*np.ones(extended_pixel_grid,dtype=np.uint8)
+
+        # 127 is mean luminance value
+        sn = 127*np.ones([T,X_extended,Y_extended],dtype=np.uint8)
+
+        for t in range(T):
+            available = np.ones(extended_pixel_grid).astype(np.bool)
+            
+            cls.exclude_threshold(available, pixel_off_count_grid, 
+                                  pixel_on_count_grid, pixel_repeats)
+
+            while np.any(available):
+                x_available, y_available = np.where(available)
+
+                pairs = zip(x_available,y_available)
+                pair_index = np.random.choice(range(len(pairs)))
+                x,y = pairs[pair_index]
+
+                p = np.random.random()
+                if p < 0.5:
+                    sn[t,x,y] = 255
+                    pixel_on_count_grid[x,y] += 1
+                else:
+                    sn[t,x,y] = 0
+                    pixel_off_count_grid[x,y] += 1
+                      
+
+                cls.exclude_region(available,(x,y),exclusion=exclusion)
+                
+        if (np.array_equal(pixel_off_count_grid, target_pixel_count) and 
+            np.array_equal(pixel_on_count_grid, target_pixel_count)):
+
+            print 'target pixel sample rate achieved'
+        else:
+            print 'target pixel sample rate not achieved. Increase number of frames'
+
+        return sn[:, buffer_x:(X+buffer_x),buffer_y:(Y+buffer_y)]
+
+
 
 class DriftingGratingCircle(Stim):
     """
