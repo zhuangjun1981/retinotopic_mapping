4ee55a0e30bfadfa048a240c962b14e8bd9fd867
zhuangjun1981 <wood_stocker@hotmail.com>
2017-09-05 11:47:02 -0700

    changed requirements.txt and setup.py to build docs

--- setup.py
+++ setup.py
@@ -13,48 +13,48 @@ install_reqs = [ir for ir in install_reqs if '#' not in ir]
 
 print install_reqs
 
-# def read(*filenames, **kwargs):
-#     encoding = kwargs.get('encoding', 'utf-8')
-#     sep = kwargs.get('sep', '\n')
-#     buf = []
-#     for filename in filenames:
-#         with io.open(filename, encoding=encoding) as f:
-#             buf.append(f.read())
-#     return sep.join(buf)
-#
-# long_description = read('README.md')
-#
-# print long_description
-#
-# def prepend_find_packages(*roots):
-#     '''
-#     Recursively traverse nested packages under the root directories
-#     '''
-#     packages = []
-#
-#     for root in roots:
-#         packages += [root]
-#         packages += [root + '.' + s for s in find_packages(root)]
-#
-#     return packages
-#
-# setup(
-#       name='retinotopic_mapping',
-#       version = '2.1.1',
-#       url='https://github.com/zhuangjun1981/retinotopic_mapping',
-#       author='Jun Zhuang @ Allen Institute for Brain Science',
-#       install_requires=install_reqs,
-#       author_email='junz@alleninstitute.org',
-#       description='retinotopic mapping tools',
-#       long_description=long_description,
-#       packages=prepend_find_packages('retinotopic_mapping'),
-#       include_package_data=True,
-#       package_data={'':['*.md']},
-#       platforms='any',
-#       classifiers = [
-#                      'Programming Language :: Python',
-#                      'Development Status :: 4 - Beta',
-#                      'Natural Language :: English',
-#                      'Operating System :: OS Independent',
-#                      ]
-#        )
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+
+print long_description
+
+def prepend_find_packages(*roots):
+    '''
+    Recursively traverse nested packages under the root directories
+    '''
+    packages = []
+
+    for root in roots:
+        packages += [root]
+        packages += [root + '.' + s for s in find_packages(root)]
+
+    return packages
+
+setup(
+      name='retinotopic_mapping',
+      version = '2.1.1',
+      url='https://github.com/zhuangjun1981/retinotopic_mapping',
+      author='Jun Zhuang @ Allen Institute for Brain Science',
+      install_requires=install_reqs,
+      author_email='junz@alleninstitute.org',
+      description='retinotopic mapping tools',
+      long_description=long_description,
+      packages=prepend_find_packages('retinotopic_mapping'),
+      include_package_data=True,
+      package_data={'':['*.md']},
+      platforms='any',
+      classifiers = [
+                     'Programming Language :: Python',
+                     'Development Status :: 4 - Beta',
+                     'Natural Language :: English',
+                     'Operating System :: OS Independent',
+                     ]
+       )
