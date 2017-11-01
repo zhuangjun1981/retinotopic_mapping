e0f55e171b144e2ec23db88c2d5fc68ee3163890
zhuangjun1981 <wood_stocker@hotmail.com>
2017-11-01 16:38:26 -0700

    still working on DisplayLogAnalysis.py, almost done.

--- retinotopic_mapping/DisplayLogAnalysis.py
+++ retinotopic_mapping/DisplayLogAnalysis.py
@@ -8,6 +8,11 @@ import numpy as np
 import tools.FileTools as ft
 
 class DisplayLogAnalyzer(object):
+    """
+    class to take display_log (.pkl) file, check its integrity and extract stimuli and display
+    organize into stim_dict dictionary, which is a intermediate step to put visual display
+    information into nwb files.
+    """
 
     def __init__(self, log_path):
 
@@ -54,6 +59,34 @@ class DisplayLogAnalyzer(object):
         return len(self.log_dict['presentation']['displayed_frames'])
 
     def get_stim_dict(self):
+        """
+        Returns
+        -------
+        stim_dict: dictionary
+            the structure of this dictionary should look like this:
+
+            {
+             '000_UniformContrastRetinotopicMapping': {
+                                                       ...
+                                                       'stim_name' : '000_UniformContrastRetinotopicMapping',
+                                                       'index_to_display': <index referencing 'frames_unique' field>
+                                                       'timestamps': <index referencing entire display sequence,
+                                                                     should match hardware vsync signal>
+                                                       'frames_unique': list of tuple representing unique frames
+                                                       ...
+                                                       },
+             '001_StimulusSeparatorRetinotopicMapping: {
+                                                        ...
+                                                        'stim_name' : '000_UniformContrastRetinotopicMapping',
+                                                        'index_to_display': <index referencing 'frames_unique' field>
+                                                        'timestamps': <index referencing entire display sequence,
+                                                                       should match hardware vsync signal>
+                                                        'frames_unique': list of tuple representing unique frames
+                                                        ...
+                                                        },
+             ...
+             }
+        """
 
         comments = ''
         description = ''
@@ -67,20 +100,24 @@ class DisplayLogAnalyzer(object):
             stim_ids.sort()
             for stim_id in stim_ids:
                 curr_dict = self.log_dict['stimulation']['individual_logs'][stim_id]
+                curr_stim_name = stim_id + 'RetinotopicMapping'
+                curr_dict['stim_name'] = curr_stim_name
                 curr_num_frames = len(curr_dict['index_to_display'])
                 curr_dict.update({'timestamps': np.arange(curr_num_frames, dtype=np.uint64) + curr_frame_ind,
                                   'comments': comments,
                                   'source': source,
                                   'description': description})
                 curr_frame_ind = curr_frame_ind + curr_num_frames
-                stim_dict.update({stim_id: curr_dict})
+                stim_dict.update({curr_stim_name: curr_dict})
 
         # if only one stimulus was displayed
         else:
             stim_name = self.log_dict['stimulation']['stim_name']
             if stim_name in ['UniformContrast', 'FlashingCircle', 'SparseNoise', 'LocallySparseNoise',
                              'DriftingGratingCirlce', 'StaticGratingCircle', 'StaticImages', 'StimulusSeparator']:
+                curr_stim_name = '{:03d}_{}RetinotopicMapping'.format(0, stim_name)
                 curr_dict = self.log_dict['stimulation']
+                curr_dict['stim_name'] = curr_stim_name
                 curr_dict.update({'timestamps': np.arange(self.num_frame_tot, dtype=np.uint64)})
             else:
                 raise NotImplementedError('Do not understand stimulus: {}.'.format(stim_name))
@@ -88,7 +125,7 @@ class DisplayLogAnalyzer(object):
             curr_dict.update({'comments': comments,
                               'source': source,
                               'description': description})
-            stim_dict.update({'000_' + stim_name: curr_dict})
+            stim_dict.update({curr_stim_name : curr_dict})
 
         return stim_dict
 
