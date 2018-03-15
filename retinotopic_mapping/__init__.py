ec09e53c2797c9782ecdeb821bb319af164e877e
zhuangjun1981 <wood_stocker@hotmail.com>
2018-03-15 11:28:04 -0700

    now `python setup.py install` works and test() at command line works.

--- retinotopic_mapping/__init__.py
+++ retinotopic_mapping/__init__.py
@@ -1,11 +1,9 @@
+import os
 __version__ = '2.6.0'
 
 def test():
     import pytest
-    import os
-
-    curr_file_path = os.path.realpath(__file__)
-    curr_file_dir = os.path.dirname(curr_file_path)
-    test_dir = os.path.join(curr_file_dir, 'test')
+    curr_dir = os.path.dirname(os.path.realpath(__file__))
+    test_dir = os.path.join(curr_dir, 'test')
     test_dir = test_dir.replace('\\', '/')
     pytest.main(test_dir)
--- setup.py
+++ setup.py
@@ -31,37 +31,6 @@ def read(*filenames, **kwargs):
     return sep.join(buf)
 long_description = read('README.md')
 
-# Recursively traverse nested packages under the root directories
-# packages = [f[0] for f in os.walk(package_root)]
-# packages = [f.replace('\\', '/') for f in packages]
-# packages = [unicode(f) for f in packages if f[-18:] != '.ipynb_checkpoints']
-
-packages = [package_root]
-packages += [package_root + '.' + s for s in find_packages(package_root)]
-packages += ['{}/examples'.format(package_root)]
-packages += ['{}/test'.format(package_root)]
-packages += ['{}/other_test'.format(package_root)]
-print('\npackages to be installed:')
-print('\n'.join(packages))
-
-# define tests
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = ['--junitxml=result.xml']
-        self.test_args_cov = self.test_args + ['--cov=retinotopic_mapping', '--cov-report=term', '--cov-report=html']
-        self.test_suite = True
-
-    def run_test(self):
-        import pytest
-
-        try:
-            errcode = pytest.main(self.test_args_cov)
-        except:
-            errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
-
-
 # setup
 setup(
       name='retinotopic_mapping',
@@ -69,11 +38,10 @@ setup(
       url='https://github.com/zhuangjun1981/retinotopic_mapping',
       author='Jun Zhuang @ Allen Institute for Brain Science',
       install_requires=install_reqs,
-      cmdclass={'test': PyTest},
       author_email='junz@alleninstitute.org',
       description='retinotopic mapping tools',
       long_description=long_description,
-      packages=packages,
+      packages=find_packages(),
       include_package_data=True,
       package_data={'':['*.md']},
       platforms='any',
